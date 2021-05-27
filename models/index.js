8b844b1d98250880501a0eb130da28915b81c0f8
InaWise <Blissful.simplicity16@gmail.com>
2021-05-26 20:58:59 -0400

    Update index.js

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+
+};
