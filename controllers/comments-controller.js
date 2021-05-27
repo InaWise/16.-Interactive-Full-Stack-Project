33b294fc0dfeff18a979895744cab332c8a40e51
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-05-26 20:59:19 -0400

    First commit for controllers

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment } = require('/models');
 
 router.get('/', (req, res) => {
 Comment.findAll()
@@ -11,13 +11,13 @@ Comment.findAll()
 });
 
 router.post('/', (req, res) => {
-  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+ 
     Comment.create({
     comment_text: req.body.comment_text,
     user_id: req.session.user_id,
     post_id: req.body.post_id,
-    upvote: ,
-    downvote: ,
+    upvote: , //placeholder
+    downvote: , //placeholder
 })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
