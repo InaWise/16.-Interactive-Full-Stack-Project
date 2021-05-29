8b9c88eecf8c55bbf174740b01f9d32a08018d21
ItsEvdok <semionaevdokimov@icloud.com>
2021-05-28 22:24:06 -0400

    Update post.js

--- models/post.js
+++ models/post.js
@@ -3,37 +3,38 @@ const { Model, DataTypes } = require("sequelize");
 // Set up a config file
 const sequelize = require("../config/connection");
 
-class Post extends Model { }
+class Post extends Model {}
 
-Post.init({
+Post.init(
+  {
     id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-},
-title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-    len: [1],
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
     },
 
-},
-
-user_id: {
-    type: DataTypes.INTEGER,
-    references: {
-    model: "user",
-    key: "id",
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
     },
-},
-},
-{
-sequelize,
+  },
+  {
+    sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: "post",
-});
+  }
+);
 
 module.exports = Post;
